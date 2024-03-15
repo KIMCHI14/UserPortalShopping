7e4c4dcf5d6f649fac7502f68766fbee7ed206c7
Kim Chi 12A7-Hoàng <hoangkimchi0590@gmail.com>
2024-03-15 15:50:56 +0700

    h

---
+++ src/component/layout.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+const items1 = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+const menu=[
+    {
+        key:1,
+        name:'User',
+        icon:UserOutlined
+    },
+    {
+        key:2,
+        name:'Product',
+        icon:UserOutlined
+    },
+    {
+        key:3,
+        name:'Order',
+        icon:UserOutlined
+    }
+]
+const items2 = menu.map((menu, index) => {
+  const key = String(index);
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(menu.icon),
+    label: menu.name,
+    children: new Array(4).fill(null).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
+const LayoutDashboard = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout>
+      <Header
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          items={items1}
+          style={{
+            flex: 1,
+            minWidth: 0,
+          }}
+        />
+      </Header>
+      <Content
+        style={{
+          padding: '0 48px',
+        }}
+      >
+        {/* <Breadcrumb
+          style={{
+            margin: '16px 0',
+          }}
+        >
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>Layout</Breadcrumb.Item>
+        </Breadcrumb> */}
+        <Layout
+          style={{
+            padding: '24px 0',
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          <Sider
+            style={{
+              background: colorBgContainer,
+            }}
+            width={200}
+          >
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              defaultOpenKeys={['sub1']}
+              style={{
+                height: '100%',
+              }}
+              items={items2}
+            />
+          </Sider>
+          <Content
+            style={{
+              padding: '0 24px',
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Content>
+      <Footer
+        style={{
+          textAlign: 'center',
+        }}
+      >
+        {/* Ant Design ©{new Date().getFullYear()} Created by Ant UED */}
+      </Footer>
+    </Layout>
+  );
+};
+export default LayoutDashboard;
